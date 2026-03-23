daf2aab20f90108e571941a6ea4e6ba48f4fba1e
Student RM4-3073 01 <studentlab1401@it.polsl.pl>
2026-03-23 16:44:47 +0100

    koniec zajec

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Second } from './second/second';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MyDirective } from './my-directive';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Second, MyDirective],
+  imports: [BrowserModule, CommonModule, AppRoutingModule, FormsModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,29 @@ import { Component, signal } from '@angular/core';
   styles: []
 })
 export class App {
-  protected readonly title = signal('lab1');
+  protected readonly title = 'lab1';
+  public licznikGlobalny: number = 0;
+  public tablica: string[] = ['Pierwszy', 'drugi', 'trzeci', 'czwarty'];
+  public nowaNazwaInput?: string;
+  public czyMoznaUsuwac: boolean = true;
+
+  onZmianaLicznika(): void {
+    this.licznikGlobalny++;
+  }
+
+  onDodajKomponentClick(): void {
+    if(this.nowaNazwaInput) {
+      this.tablica.push(this.nowaNazwaInput);
+      this.nowaNazwaInput = '';
+    } else {
+      let nowaNazwa: string = "Komponent nr ";
+      nowaNazwa += this.tablica.length;
+      this.tablica.push(nowaNazwa);
+      // this.tablica[0] = nowaNazwa;
+    }
+  }
+
+  onUsunKomponent(nazwa: string): void {
+    this.tablica = this.tablica.filter(x => x !== nazwa);
+  }
 }
